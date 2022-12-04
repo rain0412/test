b9d594bbaeedf501ffd037c8248a81ca0ff71615
rain0412 <119740346+rain0412@users.noreply.github.com>
2022-12-04 11:42:53 +0800

    Create new.c

---
+++ new.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int num,i = 0;
+  printf("This is a new file!\n");
+  while(i < 10)
+  {
+    printf("input:");
+    scanf("%c",&num);
+    i++;
+  }
+  
+  return 0;
+}
